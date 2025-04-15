ac87369c80afa4024d1e5b6afe2b3b33d09aa771
Suraj Sharma <crystinade@gmail.com>
2025-04-15 13:12:21 +0530

    post images feature

--- src/main/java/cryst/blogapi/controller/PostController.java
+++ src/main/java/cryst/blogapi/controller/PostController.java
@@ -1,24 +1,39 @@
 package cryst.blogapi.controller;
 
 
+import cryst.blogapi.config.AppConstants;
 import cryst.blogapi.entities.Post;
 import cryst.blogapi.payloads.ApiResponse;
 import cryst.blogapi.payloads.PostDto;
 import cryst.blogapi.payloads.PostResponse;
+import cryst.blogapi.service.FileService;
 import cryst.blogapi.service.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class PostController {
 
+    @Value("${project.image}")
+    private String path;
+
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private FileService fileService;
 
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId,
@@ -43,10 +58,10 @@ public class PostController {
 
     @GetMapping("/posts")
     public  ResponseEntity<PostResponse> getAllPost(
-            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false)Integer pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false)String sortDir){
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false)Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false)String sortDir){
         PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDir);
         return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
     }
@@ -73,7 +88,6 @@ public class PostController {
 
       return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
 
-
     }
 
     @GetMapping("/posts/search/{keywords}")
@@ -82,6 +96,30 @@ public class PostController {
     ){
         List<PostDto> result = this.postService.searchPosts(keywords);
         return  new ResponseEntity<List<PostDto>>(result, HttpStatus.OK);
+    }
+
+    @PostMapping("post/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(
+            @RequestParam("image")MultipartFile image,
+            @PathVariable Integer postId
+            ) throws IOException {
+
+       String fileName = this.fileService.uploadImage(path, image);
+       PostDto postDto = this.postService.getPostById(postId);
+       postDto.setImageName(fileName);
+       PostDto updatePost = this.postService.updatePost(postDto, postId);
+
+       return  new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+    }
+
+    public void downloadImage(
+            @PathVariable("imageName") String imageName,
+            HttpServletResponse response
+    ) throws IOException {
+
+        InputStream resource = this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
     }
 
 }
